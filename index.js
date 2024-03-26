ec107570f352a8f17d478b023c3a292b360d3e1a
sravanti2300 <sravanti.kanchi@gmail.com>
2024-03-26 00:34:26 -0400

    changes

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const axios = require('axios');
 const fs = require('fs').promises;
 const Handlebars = require('handlebars');
 const functions = require('@google-cloud/functions-framework');
+const mysql = require('mysql2/promise');
 
+const auth = { username: process.env.MAILGUN_USER, password: process.env.MAILGUN_PASSWORD };
 
-const auth = { username: process.env.user, password: process.env.password };
 
 
 const loadTemplate = async () => {
@@ -31,7 +32,6 @@ const sendMail = async (params) => {
       html: emailContent,
     };
     
-    console.log(formData);
     const response = await axios.post(`https://api.mailgun.net/v3/${domain}/messages`, formData, {
       auth,
       headers: {
@@ -44,12 +44,37 @@ const sendMail = async (params) => {
   }
 }
 
-functions.cloudEvent('helloPubSub', cloudEvent => {
+functions.cloudEvent('helloPubSub', async cloudEvent => {
   // The Pub/Sub message is passed as the CloudEvent's data payload.
   const base64name = cloudEvent.data.message.data;
   const buffer = Buffer.from(base64name, 'base64').toString();
   const jsonObject = JSON.parse(buffer);
-  sendMail(jsonObject);
+  await sendMail(jsonObject);
+
+  try {
+    console.log(process.env.SQL_HOST, process.env.SQL_USER, process.env.SQL_PASSWORD, process.env.SQL_DB )
+    const pool = mysql.createPool({
+      host: process.env.SQL_HOST,
+      user:  process.env.SQL_USER, 
+      password:  process.env.SQL_PASSWORD,
+      database: process.env.SQL_DB
+    });
+
+    // Get a connection from the pool
+    const connection = await pool.getConnection();
+
+    // Execute the update query
+    const updateQuery = 'INSERT INTO MailTrackings (email, mail_sent) VALUES (?, CURRENT_TIMESTAMP()) ON DUPLICATE KEY UPDATE mail_sent = CURRENT_TIMESTAMP();';
+    await connection.execute(updateQuery, [jsonObject.to]);
+    await connection.release();
+    console.log('Mail sent field updated successfully for user:', jsonObject.to);  
+    console.log('Database connected successfully!');
+
+  } catch(err) {
+    console.error('Error connecting to the database:', err);
+
+  }
+
 });
 
 
